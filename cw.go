54635257c4e25685f485a11e702a7e960a01c5ed
Sylvan <sylvanchouhan@gmail.com>
2021-08-15 13:30:39 +0100

    Complete DigitalRoot

---
+++ cw.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func DigitAdder(n int) int {
+	remainder := 0
+	sum := 0
+	for n !=0 {
+		remainder = n%10
+		sum += remainder
+		n=n/10
+	}
+	return sum
+}
+
+func DigitalRoot(n int) int {
+	for n>9 {
+		n=DigitAdder(n)
+	}
+	return n
+}
+
+func main() {
+	for {
+		var n int
+		fmt.Println("Enter number: ")
+		fmt.Scanf("%d", &n)
+		fmt.Println(DigitalRoot(n))
+	}
+}
---
+++ digitsplitter.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func DigitAdder(n int) int {
+	return
+}
+
+func DigitalRoots(n int) int {
+	for {
+		
+
+		if n<10 {
+			break
+		}
+	}
+}
